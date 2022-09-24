33d7594761eb13a018a47303fb73e6c55c732063
wahek <wahek1999@mail.ru>
2022-09-24 20:33:42 +0300

    Задание 3

--- Program.cs
+++ Program.cs
@@ -17,24 +17,31 @@
 
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
-Console.WriteLine("Последовательно введите координаты точки 1: ");
-int x0 = int.Parse(Console.ReadLine());
-int y0 = int.Parse(Console.ReadLine());
-int z0 = int.Parse(Console.ReadLine());
-Console.WriteLine("Последовательно введите координаты точки 2: ");
-int x1 = int.Parse(Console.ReadLine());
-int y1 = int.Parse(Console.ReadLine());
-int z1 = int.Parse(Console.ReadLine());
+// Console.WriteLine("Последовательно введите координаты точки 1: ");
+// int x0 = int.Parse(Console.ReadLine());
+// int y0 = int.Parse(Console.ReadLine());
+// int z0 = int.Parse(Console.ReadLine());
+// Console.WriteLine("Последовательно введите координаты точки 2: ");
+// int x1 = int.Parse(Console.ReadLine());
+// int y1 = int.Parse(Console.ReadLine());
+// int z1 = int.Parse(Console.ReadLine());
 
-if (x0 == 0 && y0 == 0 && z0 == 0 && x1 == 0 && y1 == 0 && z1 == 0) Console.Write("Обе точки вначале координат");
+// if (x0 == 0 && y0 == 0 && z0 == 0 && x1 == 0 && y1 == 0 && z1 == 0) Console.Write("Обе точки вначале координат");
 
-else{
-double distance = Math.Sqrt((Math.Pow(x1 - x0, 2)) + Math.Pow(y1 - y0, 2) + Math.Pow(z1 - z0, 2));
-Console.Write($"Расстояние между точками = {distance}");
-}
+// else{
+// double distance = Math.Sqrt((Math.Pow(x1 - x0, 2)) + Math.Pow(y1 - y0, 2) + Math.Pow(z1 - z0, 2));
+// Console.Write($"Расстояние между точками = {distance}");
+// }
 // Задача 23
 
 // Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+for (int i = 0; i < num; i++ ){
+    double sqr = Math.Pow(i, 3);
+    Console.Write(sqr+" ");
+}
